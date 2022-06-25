5209a0437e08181ade73be83967e7b977fc41a90
matousss <matousss33@gmail.com>
2022-06-25 17:25:21 +0200

    Improvements

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -2,6 +2,9 @@ import {Map} from "../Map";
 import {Page} from "./Page";
 import './Contact.css'
 
+const EMAIL = 'lkijovska@gmail.com'
+const TELEPHONE = '420 608 292 491'
+
 export function Contact() {
     return (
         <Page className={'page w-75 mx-auto pt-4'}>
@@ -11,11 +14,13 @@ export function Contact() {
             <div className={'border border-2 border-red mt-3 row rounded-2'}>
 
                 <div className={'col-md-4 p-0'}>
-                    <div className={'p-4 p-md-5 text-md-start text-center'}>
+                    <div className={'p-4 p-md-4 p-xl-5 text-md-start text-center contacts'}>
                         <nobr>
                             Lenka Hájková<br/>
-                            lkijovska@gmail.com<br/>
-                            +420 608 292 491<br/>
+                            <a href={'mailto:' + EMAIL}>{EMAIL}</a><br/>
+                            <a href={'tel:' + TELEPHONE.replace(' ', '')}>
+                                +{TELEPHONE}
+                            </a><br/>
                             IČO 11367920<br/>
                             <br/>
                             Plzeňská 61,<br/>
@@ -25,7 +30,7 @@ export function Contact() {
                     </div>
 
                 </div>
-                <div className={'row-cols-md-1 col-md-8 p-0'}>
+                <div className={'row-cols-md-1 col-md-8 col-xl-8 p-0'}>
                     <Map className={'w-100 h-100'}/>
                 </div>
 
